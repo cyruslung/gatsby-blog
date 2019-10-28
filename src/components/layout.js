6eaba877cc6fa6338d677e5a5f1b26382ae6bff0
linsheng24 <linsheng24@gmail.com>
2019-10-28 21:38:32 +0800

    Day23 complete index and post-template components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,20 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import 'bootstrap/dist/css/bootstrap.css'
+import '../scss/clean-blog.scss'
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+const Layout = (props) => (
+  <>
+    {props.children}
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 col-md-10 mx-auto">
+            <p className="copyright text-muted">Copyright &copy; Your Website 2019</p>
+          </div>
+        </div>
       </div>
-    </>
-  )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
+    </footer>
+  </>
+)
 export default Layout
--- src/components/post-template.js
+++ src/components/post-template.js
@@ -1,34 +1,8 @@
 import React from "react"
-
+import Layout from "./layout"
 
 const PostPage = () => (
-  <>
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div className="container">
-        <a className="navbar-brand" href="index.html">Start Bootstrap</a>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i className="fas fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="index.html">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="about.html">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="post.html">Sample Post</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="contact.html">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
+  <Layout>
     <header className="masthead" style={{backgroundImage: "url('img/post-bg.jpg')"}}>
       <div className="overlay"></div>
       <div className="container">
@@ -94,43 +68,7 @@ const PostPage = () => (
 
     <hr/>
 
-    <footer>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-8 col-md-10 mx-auto">
-            <ul className="list-inline text-center">
-              <li className="list-inline-item">
-                <a href="#">
-                  <span className="fa-stack fa-lg">
-                    <i className="fas fa-circle fa-stack-2x"></i>
-                    <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                  </span>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#">
-                  <span className="fa-stack fa-lg">
-                    <i className="fas fa-circle fa-stack-2x"></i>
-                    <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                  </span>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#">
-                  <span className="fa-stack fa-lg">
-                    <i className="fas fa-circle fa-stack-2x"></i>
-                    <i className="fab fa-github fa-stack-1x fa-inverse"></i>
-                  </span>
-                </a>
-              </li>
-            </ul>
-            <p className="copyright text-muted">Copyright &copy; Your Website 2019</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-
-  </>
+  </Layout>
 )
 
 export default PostPage
